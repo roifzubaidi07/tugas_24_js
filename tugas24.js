9a4c5ec356c7bcf79c0d964a2dad3d13e183dc8d
M. Ro'if <roifzubaidi5@gmail.com>
2021-02-21 23:59:30 +0700

    Pengumpulan tugas 24 NIOMIC

---
+++ tugas24.js
@@ -0,0 +1,5 @@
+let angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log('Sebelumnya : ' + angka.join(','));
+console.log('Ascending : ' + angka.sort().join(','));
+console.log('Ascending : ' + angka.reverse().join(','));
